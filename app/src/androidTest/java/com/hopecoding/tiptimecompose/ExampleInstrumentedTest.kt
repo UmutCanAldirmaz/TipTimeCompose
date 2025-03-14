8656100eefad6c8f6ca780c684ba3147f1eb26b9
Umut Can Aldırmaz <ucan.aldirmaz@gmail.com>
2025-03-14 21:11:57 +0300

    Local and instrumentation tests were written for the application.

--- app/src/androidTest/java/com/hopecoding/tiptimecompose/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hopecoding/tiptimecompose/ExampleInstrumentedTest.kt
@@ -1,12 +1,17 @@
 package com.hopecoding.tiptimecompose
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.view.Surface
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.hopecoding.tiptimecompose.ui.theme.TipTimeComposeTheme
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import java.text.NumberFormat
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +20,25 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hopecoding.tiptimecompose", appContext.packageName)
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipTimeComposeTheme {
+                TipTimeLayout()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+        )
     }
+
+
 }
